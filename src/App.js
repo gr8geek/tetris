d82ef5498986ebd6cabd74047d8a62b5ff6ea28d
gr8geek <pratyush9000@gmail.com>
2020-06-13 22:02:27 +0530

    Todo list created

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
-import Joke from './components/Joke';
-import jokeData from './components/jokeData';
+import TodoItem from './components/TodoItem';
 function App(){
-  const nums =[1,2,3,4,5,6,7,8,9,10];
-  const doubled=nums.map((num)=> num*2);
-  console.log("new array=",doubled);
-  const JokeList=jokeData.map((joke)=>{
-    return(
-      <Joke key={joke.id} quest={joke.quest} punch={joke.punch}/>
-    );
-  });
-
+  
   return(
-    <div>
-      {JokeList}
+    <div className="todo-list">
+      <TodoItem label="Palce Holder Text 1"/>
+      <TodoItem label="Palce Holder Text 1"/>
+      <TodoItem label="Palce Holder Text 1"/>
+      <TodoItem label="Palce Holder Text 1"/>
     </div>
   );
 }
