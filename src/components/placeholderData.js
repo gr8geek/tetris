1ea44a490226838db688a7a1737d4c6696612b6e
gr8geek <pratyush9000@gmail.com>
2020-06-13 22:23:29 +0530

    Added placeholder data

---
+++ src/components/placeholderData.js
@@ -0,0 +1,17 @@
+placeholderData=[
+    {
+        id:0,
+        text:"Option 1",
+        checked:false,
+    },
+    {
+        id:1,
+        text:"Option 1",
+        checked:false,
+    },
+    {
+        id:2,
+        text:"Option 1",
+        checked:false,
+    },
+]
